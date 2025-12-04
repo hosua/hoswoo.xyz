dc37b63b1c9d7b106131f6ca3eec96a6bc39e489
hosua <hosua.o@gmail.com>
2025-12-04 04:45:20 -0500

    Add ThemeSwitcher

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { ThemeProvider } from "@/components/theme-provider";
 import GamesPage from "@/pages/GamesPage";
+import ThemeSwitcher from "./components/ThemeSwitcher";
 
 function App() {
   return (
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+    <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+      <ThemeSwitcher />
       <GamesPage />
     </ThemeProvider>
   );
---
+++ src/components/ThemeSwitcher.tsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import { LaptopMinimalIcon, MoonIcon, SunIcon } from "lucide-react";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
+import { useTheme } from "@/components/theme-provider";
+
+export const ThemeSwitcher = () => {
+  const [open, setOpen] = useState(false);
+  const { theme, setTheme } = useTheme();
+
+  const getIcon = () => {
+    if (theme === "dark") return <MoonIcon />;
+    if (theme === "light") return <SunIcon />;
+    return <LaptopMinimalIcon />;
+  };
+
+  return (
+    <DropdownMenu open={open} onOpenChange={setOpen}>
+      <DropdownMenuTrigger asChild>
+        <Button size="icon" variant="outline">
+          {getIcon()}
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuItem onClick={() => setTheme("system")}>
+          <LaptopMinimalIcon className="mr-2 h-4 w-4" /> System
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("light")}>
+          <SunIcon className="mr-2 h-4 w-4" /> Light
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("dark")}>
+          <MoonIcon className="mr-2 h-4 w-4" /> Dark
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+export default ThemeSwitcher;
